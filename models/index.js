df82557235053a8aae4c14a46c496e04248c9a1e
álvaro antonio <alvaroantonioluz@gmail.com>
2023-03-13 17:34:48 -0300

    mongo: handling errors

--- models/index.js
+++ models/index.js
@@ -1,7 +1,12 @@
+const errorHandler = require('./handlers/error')
+
 const mongoose = require('mongoose');
 mongoose.Promise = Promise;
 mongoose.connect(process.env.MONGODB_URI, err => {
-		if (err) throw err;
+		if (err) {
+			// throw err;
+			errorHandler(err, req, res, next)
+		}
 		console.log('Connected to MongoDB.')
 	});
 
